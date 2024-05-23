ecb6655c64343bc96f1cd898eb6acd761f2b6386
Edsamn <eduardomsribas@gmail.comm>
2024-05-23 20:09:05 -0300

    termina emprestar livro

--- BLOCO-INTERMEDIÁRIO/POO/gerenciador-biblioteca/src/model/Usuario.ts
+++ BLOCO-INTERMEDIÁRIO/POO/gerenciador-biblioteca/src/model/Usuario.ts
@@ -1,20 +1,24 @@
 import Livro from "./Livro";
 
 class Usuario {
-    private nome: string;
-    private id: number;
-    private livroEmprestado: Livro[];
-    
-    constructor(nome: string, id: number) {
-        this.nome = nome;
-        this.id = id;
-    }
+  private nome: string;
+  private id: number;
+  private livroEmprestado: Livro[];
 
-    emprestarLivro() {
-        
-    }
+  constructor(nome: string, id: number) {
+    this.nome = nome;
+    this.id = id;
+    this.livroEmprestado = [];
+  }
 
-    devolverLivro() {
-        this.devolverLivro();
-    }
+  emprestarLivro(livro: Livro) {
+    this.livroEmprestado.push(livro);
+    livro.emprestar();
+  }
+
+  devolverLivro() {
+    this.devolverLivro();
+  }
 }
+
+export default Usuario;
