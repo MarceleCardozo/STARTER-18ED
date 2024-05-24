706d240c8772b945da969bfa9f9b8bc6a7d4de95
Douglas Rosa da Silva <douglasrosaadvocacia@gmail.com>
2024-05-23 21:32:04 -0300

    achamos la biblioteca

--- BLOCO-INTERMEDIÁRIO/POO/gerenciador-biblioteca/src/model/Biblioteca.ts
+++ BLOCO-INTERMEDIÁRIO/POO/gerenciador-biblioteca/src/model/Biblioteca.ts
@@ -9,4 +9,16 @@ class Biblioteca {
         this.livros = livros;
         this.usuarios = usuarios;
     }
+
+    registrarUsuario(usuario:Usuario){
+        this.usuarios.push(usuario)
+    }
+
+    listarLivrosDisponiveis(){
+        console.log(this.livros);
+    }
+
+    buscarLivroPorTitulo(titulo:string){
+        this.livros.forEach(livro => this.titulo === titulo)
+    }
 }
