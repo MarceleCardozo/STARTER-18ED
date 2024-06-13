83a80d9d43f4529bc358db7cc0a2c8b52fa9b1a9
Edsamn <eduardomsribas@gmail.comm>
2024-06-13 20:43:32 -0300

    nomes das pastas corrigidos

--- BLOCO-INTERMEDIÁRIO/REACT-I/landing-page/src/Routes/AppRoutes.tsx
+++ BLOCO-INTERMEDIÁRIO/REACT-I/landing-page/src/Routes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {RouterProvider, createBrowserRouter} from "react-router-dom";
 import Home from "../pages/Home";
 
 const router = createBrowserRouter([
