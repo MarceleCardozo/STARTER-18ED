5a93cc6799edd1ff44db767f17f05a11f22d6e5b
Cristiane ferreira <crislh2017@gmail.com>
2024-06-13 19:53:25 -0300

    adisciona header

---
+++ BLOCO-INTERMEDIÁRIO/REACT-I/landing-page/src/Pages/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface ComponetNameProps {
+  property: string;
+}
+
+const ComponetName: React.FC<ComponetNameProps> = ({ property }) => {
+  return (
+    <>
+      <h1>Growdev</h1>
+      
+    </>
+  );
+};
+
+export default ComponetName;
