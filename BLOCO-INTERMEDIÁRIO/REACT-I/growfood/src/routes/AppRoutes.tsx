0fa008ab2e25e03a1083aefa6f3e953783ec6068
Joel Luis Steffen <jo.lsteffen@gmail.com>
2024-06-18 20:15:31 -0300

    criacao de navbar

--- BLOCO-INTERMEDIÁRIO/REACT-I/growfood/src/routes/AppRoutes.tsx
+++ BLOCO-INTERMEDIÁRIO/REACT-I/growfood/src/routes/AppRoutes.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "../pages/Home";
+import Menu from "../pages/Menu";
 
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Home />,
+    },    
+    {
+      path: "/menu",
+      element: <Menu />,
     },
   ]);
   
