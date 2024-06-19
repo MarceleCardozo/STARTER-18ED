4fe25252c1f27eb4e26f0304ca6dc7ea119bb819
Edsamn <eduardomsribas@gmail.comm>
2024-06-18 22:30:40 -0300

    feat: add botão e search bar estilizados, e texto

---
+++ BLOCO-INTERMEDIÁRIO/REACT-I/growfood/src/components/search-bar/SearchBarStyled.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const SearchBarStyled = styled.input`
+  height: 42px;
+  width: 500px;
+`;
+
+export default SearchBarStyled;
--- BLOCO-INTERMEDIÁRIO/REACT-I/growfood/src/components/welcome-component/WelcomeComponent.tsx
+++ BLOCO-INTERMEDIÁRIO/REACT-I/growfood/src/components/welcome-component/WelcomeComponent.tsx
@@ -1,18 +1,32 @@
+import ButtonStyled from "../button/ButtonStyled";
+import SearchBarStyled from "../search-bar/SearchBarStyled";
 import DivWelcomeStyled from "./DivWelcomeStyle";
 import H1Styled from "./H1Styled";
 
 function WelcomeComponent() {
   return (
-    <DivWelcomeStyled>
-      <H1Styled switchColor={false}>
-        Welcome
-        <H1Styled switchColor={true}>
-          &ensp;to <br />
-          The world of <br />
-          Tasty & Fresh Food.
-        </H1Styled>
-      </H1Styled>
-    </DivWelcomeStyled>
+    <>
+      <DivWelcomeStyled>
+        <div style={{display: "flex", flexDirection: "column"}}>
+          <H1Styled switchColor={false}>
+            Welcome
+            <H1Styled switchColor={true}>
+              &ensp;to <br />
+              The world of <br />
+              Tasty & Fresh Food.
+            </H1Styled>
+          </H1Styled>
+          <p style={{fontSize: "1.2rem"}}>
+            Keep it easy with these simple but delicious recipes. <br /> From make-ahead lunches and midweek meals to <br /> fuss-free
+            sides.
+          </p>
+          <div style={{paddingLeft: "30px", paddingTop: "30px"}}>
+            <SearchBarStyled type="text" placeholder="Search, Burger" />
+            <ButtonStyled>Search</ButtonStyled>
+          </div>
+        </div>
+      </DivWelcomeStyled>
+    </>
   );
 }
 
